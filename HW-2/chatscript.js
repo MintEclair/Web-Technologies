ad86a57c991c1ff7c62498f6bcc698829b1ce575
Mint Pitassy <143541126+MintEclair@users.noreply.github.com>
2025-01-27 19:30:32 -0700

    Yes

---
+++ HW-2/chatscript.js
@@ -0,0 +1,21 @@
+// Log 3 favorite websites
+console.log("My favorite websites:");
+console.log("1. https://www.wikipedia.org - A great resource for learning.");
+console.log("2. https://www.youtube.com - My go-to for videos and tutorials.");
+console.log("3. https://www.github.com - A place to explore and share code.");
+
+// Log 3 favorite games
+console.log("My favorite games:");
+console.log("1. Chess - A timeless strategy game.");
+console.log("2. Settlers of Catan - A fun board game for trading and strategy.");
+console.log("3. Stardew Valley - A relaxing farming simulation game.");
+
+// Log 3 favorite artists
+console.log("My favorite artists and their cultural importance:");
+console.log("1. Vincent van Gogh - Known for his post-impressionist masterpieces.");
+console.log("2. Beyoncé - An influential global music icon and cultural leader.");
+console.log("3. Hayao Miyazaki - Creator of timeless animated films.");
+
+// Create a simple question and answer using prompt
+let favoriteColor = prompt("What is your favorite color?");
+console.log(`Your favorite color is: ${favoriteColor}`);
